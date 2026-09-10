0497a3b009b9570f56295ff38855282d5118fd5f
Angel Jefferson Sánchez Ventura <aj.sanchez@caid.gob.do>
2026-09-10 14:16:58 -0400

    Sincronizacion total: subiendo archivos actuales y limpiando repositorio remoto

--- CapaPresentacionAdmin/App_Start/BundleConfig.cs
+++ CapaPresentacionAdmin/App_Start/BundleConfig.cs
@@ -4,8 +4,7 @@ using System.Web.Optimization;
 namespace CapaPresentacionAdmin
 {
     public class BundleConfig
-    {
-        // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
+    {     
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new Bundle("~/bundles/jquery").Include(
@@ -19,22 +18,14 @@ namespace CapaPresentacionAdmin
                         "~/Scripts/sweetalert.min.js",
                         "~/Scripts/jquery.validate.js",
                         "~/Scripts/jquery-ui.js",
-
-
                         "~/Scripts/scripts.js"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            //            "~/Scripts/jquery.validate*"));
-
-            //// Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información sobre los formularios.  De esta manera estará
-            //// para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
-            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-            //            "~/Scripts/modernizr-*"));
 
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.bundle.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap.min.css",
                 "~/Content/site.css",
                 "~/Content/DataTables/css/responsive.dataTables.css",
                 "~/Content/sweetalert.css",
